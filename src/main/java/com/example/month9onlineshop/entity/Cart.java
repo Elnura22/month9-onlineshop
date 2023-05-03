6dd4816b8bf4b5141ecf4992e853d9db96007ae1
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-03 19:56:55 +0600

    create new tables

---
+++ src/main/java/com/example/month9onlineshop/entity/Cart.java
@@ -0,0 +1,19 @@
+package com.example.month9onlineshop.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item_id;
+
+}
---
+++ src/main/java/com/example/month9onlineshop/entity/Order.java
@@ -0,0 +1,25 @@
+package com.example.month9onlineshop.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime date;
+    @Column(length = 128)
+    private String address;
+    @Column(length = 128)
+    private String postIndex;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user_id;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item_id;
+
+    private Long totalSum;
+}
