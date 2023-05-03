4dc42a5e283694013e3a97f4a9e7f1b609c87be5
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-03 19:57:21 +0600

    change these classes, add new entities

--- src/main/java/com/example/month9onlineshop/entity/Item.java
+++ src/main/java/com/example/month9onlineshop/entity/Item.java
@@ -10,7 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 public class Item {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 128)
     private String name;
--- src/main/java/com/example/month9onlineshop/entity/User.java
+++ src/main/java/com/example/month9onlineshop/entity/User.java
@@ -10,7 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 128)
     private String accountName;
