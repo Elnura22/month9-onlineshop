22fb24e99c689f1a97e0b6d75260f8279969af6b
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-04 17:14:50 +0600

    add controllers

---
+++ src/main/java/com/example/month9onlineshop/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.month9onlineshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
