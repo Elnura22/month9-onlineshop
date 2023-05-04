cb56b6c46f90e8636261dfafb08a0c1e58d09f02
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-04 15:21:31 +0600

    add user repository

---
+++ src/main/java/com/example/month9onlineshop/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.month9onlineshop.repositories;
+
+import com.example.month9onlineshop.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
