b8fad9f706ba6e9e8426df94f9ca315453cddb35
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-08 18:36:47 +0600

    add new migration

--- src/main/resources/db/migration/V1008__create_table_cartItem_setup.sql
+++ src/main/resources/db/migration/V1008__create_table_cartItem_setup.sql
@@ -11,3 +11,15 @@ create table cart_items
 alter table if exists cart_items add constraint cart_id foreign key (cart_id) references carts;
 
 
+insert into reviews(text, user_id, date, item_id)
+values ('good', 1, '2023-05-04 16:43:32.000000', 2),
+       ('bad', 1, '2023-05-04 16:43:32.000000', 1),
+       ('ok', 2, '2023-05-04 16:43:32.000000', 1),
+       ('good', 2, '2023-05-04 16:43:32.000000', 2);
+
+
+insert into cart_items(cart_id, item_id, quantity)
+values (1, 1, 2),
+       (2, 2, 2),
+       (3, 3, 3),
+       (1, 1, 1);
