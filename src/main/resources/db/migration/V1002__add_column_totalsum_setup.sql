b9819dc6c7b518c5c28d482ed985a4a65e12ee40
Elnura Amanturova <elnura.amantur@gmail.com>
2023-05-25 17:36:26 +0600

    add new table

--- src/main/resources/db/migration/V1002__add_column_totalsum_setup.sql
+++ src/main/resources/db/migration/V1002__add_column_totalsum_setup.sql
@@ -12,13 +12,12 @@ create table orders
     date       timestamp without time zone NOT NULL,
     post_index bigint,
     total_sum  bigint,
-    cart_id    bigint,
+    user_id    bigint,
     primary key (id)
 );
 
 alter table if exists carts add constraint FKn4foj467c1dwfb0caomaqkv5l foreign key (user_id) references usr;
 
-alter table if exists orders add constraint cart_id foreign key (cart_id) references carts;
--- alter table if exists orders add constraint FK7ncuqw9n77odylknbo8aikc9w foreign key (user_id) references usr;
+alter table if exists orders add constraint user_id foreign key (user_id) references usr;
 
 
--- src/main/resources/db/migration/V1006__insert_table_carts_setup.sql
+++ src/main/resources/db/migration/V1006__insert_table_carts_setup.sql
@@ -2,6 +2,26 @@ insert into carts(user_id)
 values (1),
        (2);
 
-insert into orders(address, date, post_index, total_sum, cart_id)
+
+
+insert into orders(address, date, post_index, total_sum, user_id)
 values ('manasa 68', '2023-05-04 16:43:32.000000', '720060', 200, 1),
        ('10th microdistrict 40', '2023-05-04 16:43:32.000000', '588002', 100, 2);
+
+
+
+create table ordered_items
+(
+    id       bigserial not null,
+    order_id bigint,
+    item_id  bigint,
+    primary key (id)
+);
+
+alter table if exists ordered_items add constraint order_id foreign key (order_id) references orders;
+alter table if exists ordered_items add constraint item_id foreign key (item_id) references items;
+
+
+
+insert into ordered_items(order_id, item_id)
+values (1, 1);
